a01ee7da58bbce05cd48b0ad1e89590dfaf444fb
Erikson Lima <eriksonlima@gmail.com>
2023-09-19 16:50:31 -0400

    coletando data subnets

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,12 +1,20 @@
+data "aws_subnet" "subnet_priv_1" {
+  id = aws_subnet.subnet_priv_1.id
+}
+
+data "aws_subnet" "subnet_priv_2" {
+  id = aws_subnet.subnet_priv_2.id
+}
+
 resource "aws_instance" "ec2_test_1" {
-  subnet_id     = aws_subnet.subnet_priv_1.id
+  subnet_id     = data.aws_subnet.subnet_priv_1
   ami           = "ami-04cb4ca688797756f"
   instance_type = "t2.micro"
   key_name      = "erikson"
 }
 
 resource "aws_instance" "ec2_test_2" {
-  subnet_id     = aws_subnet.subnet_priv_2.id
+  subnet_id     = data.aws_subnet.subnet_priv_2
   ami           = "ami-04cb4ca688797756f"
   instance_type = "t2.micro"
   key_name      = "erikson"
