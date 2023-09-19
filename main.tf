60bbabb3f13143bab9aeb406f7df7f000ece47a4
Erikson Lima <eriksonlima@gmail.com>
2023-09-19 19:57:17 -0400

    reestrutuando

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "lab-project-1"
+    key    = "infra/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
