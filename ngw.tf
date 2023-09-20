0a77fc4453c7b975e7d0aa555a7ee1b24d4e7aac
Erikson Lima <eriksonlima@gmail.com>
2023-09-20 12:28:55 -0400

    corrigindo ngw

--- ngw.tf
+++ ngw.tf
@@ -1,17 +1,17 @@
-resource "aws_eip" "nat_gateway_1" {
-  instance = aws_nat_gateway.ngw_1.id
+resource "aws_eip" "ngw_1" {
+  domain = "vpc"
 }
 
-resource "aws_eip" "nat_gateway_2" {
-  instance = aws_nat_gateway.ngw_2.id
+resource "aws_eip" "ngw_2" {
+  domain = "vpc"
 }
 
 resource "aws_nat_gateway" "ngw_1" {
-  allocation_id = aws_eip.nat_gateway_1.id
+  allocation_id = aws_eip.ngw_1.id
   subnet_id     = aws_subnet.subnet_pub_1.id
 }
 
 resource "aws_nat_gateway" "ngw_2" {
-  allocation_id = aws_eip.nat_gateway_2.id
+  allocation_id = aws_eip.ngw_2.id
   subnet_id     = aws_subnet.subnet_pub_2.id
 }
