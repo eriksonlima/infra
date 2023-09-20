714b252035ca58d5ba390ee84470a9bbfcd7f2b8
Erikson Lima <eriksonlima@gmail.com>
2023-09-20 12:14:01 -0400

    corrigindo ngw e route

--- ngw.tf
+++ ngw.tf
@@ -1,9 +1,17 @@
-resource "aws_nat_gateway" "ngw_priv_1" {
-  subnet_id         = aws_subnet.subnet_priv_1.id
-  connectivity_type = "private"
+resource "aws_eip" "nat_gateway_1" {
+  instance = aws_nat_gateway.ngw.id
 }
 
-resource "aws_nat_gateway" "ngw_priv_2" {
-  subnet_id         = aws_subnet.subnet_priv_2.id
-  connectivity_type = "private"
+resource "aws_eip" "nat_gateway_2" {
+  instance = aws_nat_gateway.ngw.id
+}
+
+resource "aws_nat_gateway" "ngw_1" {
+  allocation_id = aws_eip.nat_gateway_1.id
+  subnet_id     = aws_subnet.subnet_pub_1.id
+}
+
+resource "aws_nat_gateway" "ngw_2" {
+  allocation_id = aws_eip.nat_gateway_2.id
+  subnet_id     = aws_subnet.subnet_pub_2.id
 }
