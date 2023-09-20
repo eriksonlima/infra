a2c521c8e69fa261c478580b0e610a21862412bf
Erikson Lima <eriksonlima@gmail.com>
2023-09-19 20:43:08 -0400

    reestruturando

--- network/igw.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "igw_pub" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    "Name" = "igw_pub"
-  }
-}
--- network/ngw.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_nat_gateway" "ngw_priv_1" {
-  subnet_id         = aws_subnet.subnet_priv_1.id
-  connectivity_type = "private"
-}
-
-resource "aws_nat_gateway" "ngw_priv_2" {
-  subnet_id         = aws_subnet.subnet_priv_2.id
-  connectivity_type = "private"
-}
